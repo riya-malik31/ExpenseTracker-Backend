5908bbcfae1c4fa5f345966dce74351c58ff171f
Riya Malik <riyacws123@gmail.com>
2025-05-07 15:02:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json())
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://expense-tracker-frontend-73gu.vercel.app","http://localhost:3000","https://expense-tracker-frontend-73gu.vercel.app","*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST,PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   if (req.method === 'OPTIONS') {
